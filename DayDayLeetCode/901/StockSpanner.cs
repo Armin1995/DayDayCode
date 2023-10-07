567a24c55f2682e6dc9bde391403b0741a40dfe1
Armin君 <aichenzhupo127@163.com>
2023-10-07 23:50:49 +0800

    901股票价格跨度(单调栈）

---
+++ DayDayLeetCode/901/StockSpanner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://leetcode.cn/problems/online-stock-span/solutions/1906765/gu-piao-jie-ge-kua-du-by-leetcode-soluti-5cm7/
+namespace DayDayLeetCode._901
+{
+    public class StockSpanner
+    {
+        private Stack<Tuple<int, int>> stack;
+        private int idx;
+
+        public StockSpanner()
+        {
+            stack = new Stack<Tuple<int, int>>();
+            stack.Push(new Tuple<int, int>(-1, int.MaxValue));
+            idx = -1;
+        }
+
+        public int Next(int price)
+        {
+            idx++;
+            while (price >= stack.Peek().Item2)
+            {
+                stack.Pop();
+            }
+            int ret = idx - stack.Peek().Item1;
+            stack.Push(new Tuple<int, int>(idx, price));
+            return ret;
+        }
+    }
+}
---
+++ DayDayLeetCode/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DayDayLeetCode._901;
+
+Console.WriteLine("Hello, World!");
+
+StockSpanner stockSpanner = new StockSpanner();
+Console.WriteLine(stockSpanner.Next(100));
+Console.WriteLine(stockSpanner.Next(80));
+Console.WriteLine(stockSpanner.Next(60));
+Console.WriteLine(stockSpanner.Next(70));
+Console.WriteLine(stockSpanner.Next(60));
+Console.WriteLine(stockSpanner.Next(75));
+Console.WriteLine(stockSpanner.Next(85));
+
+Console.ReadKey();
