9e80ba81c042344f8a3994479ba63329b069dead
Armin君 <aichenzhupo127@163.com>
2023-10-08 22:57:10 +0800

    2034：哈希表+有序列表

---
+++ DayDayLeetCode/2034/StockPrice.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://leetcode.cn/problems/stock-price-fluctuation/solutions/1227200/c-dictionary-sorted-list-by-ding_ding_di-jacj/
+namespace DayDayLeetCode._2034
+{
+    public class StockPrice
+    {
+        private Dictionary<int, int> _prices;
+        private SortedList _sortedList;
+        private int _lastIndex;
+
+        public StockPrice()
+        {
+            this._sortedList = new SortedList();
+            this._prices = new Dictionary<int, int>();
+            this._lastIndex = 0;
+        }
+
+        public void Update(int timestamp, int price)
+        {
+            if (this._prices.ContainsKey(timestamp))
+            {
+                var previousValue = this._prices[timestamp];
+
+                this.RemoveOrDeductValueFromSortedList(this._sortedList, previousValue);
+            }
+
+            this._prices[timestamp] = price;
+            this._lastIndex = Math.Max(timestamp, this._lastIndex);
+
+            this.AddValueFromSortedList(this._sortedList, price);
+        }
+
+        public int Current() => this._prices[this._lastIndex];
+
+        public int Maximum() => (int)this._sortedList.GetKey(this._sortedList.Count - 1);
+
+        public int Minimum() => (int)this._sortedList.GetKey(0);
+
+        private void RemoveOrDeductValueFromSortedList(SortedList sortedList, int value)
+        {
+            if (sortedList.ContainsKey(value))
+            {
+                var index = sortedList.IndexOfKey(value);
+
+                int remainingCountOfKey = (int)sortedList.GetByIndex(index);
+
+                if (remainingCountOfKey == 1)
+                {
+                    sortedList.RemoveAt(index);
+                }
+                else
+                {
+                    sortedList.SetByIndex(index, remainingCountOfKey - 1);
+                }
+            }
+        }
+
+        private void AddValueFromSortedList(SortedList sortedList, int key)
+        {
+            if (sortedList.ContainsKey(key))
+            {
+                var index = sortedList.IndexOfKey(key);
+                int remainingCountOfKey = (int)sortedList.GetByIndex(index);
+                sortedList.SetByIndex(index, remainingCountOfKey + 1);
+            }
+            else
+            {
+                sortedList.Add(key, 1);
+            }
+        }
+    }
+
+}
--- DayDayLeetCode/Program.cs
+++ DayDayLeetCode/Program.cs
@@ -1,15 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DayDayLeetCode._2034;
 using DayDayLeetCode._901;
 
 Console.WriteLine("Hello, World!");
 
-StockSpanner stockSpanner = new StockSpanner();
-Console.WriteLine(stockSpanner.Next(100));
-Console.WriteLine(stockSpanner.Next(80));
-Console.WriteLine(stockSpanner.Next(60));
-Console.WriteLine(stockSpanner.Next(70));
-Console.WriteLine(stockSpanner.Next(60));
-Console.WriteLine(stockSpanner.Next(75));
-Console.WriteLine(stockSpanner.Next(85));
+#region 901
+//{
+//    StockSpanner stockSpanner = new StockSpanner();
+//    Console.WriteLine(stockSpanner.Next(100));
+//    Console.WriteLine(stockSpanner.Next(80));
+//    Console.WriteLine(stockSpanner.Next(60));
+//    Console.WriteLine(stockSpanner.Next(70));
+//    Console.WriteLine(stockSpanner.Next(60));
+//    Console.WriteLine(stockSpanner.Next(75));
+//    Console.WriteLine(stockSpanner.Next(85));
+//}
+#endregion
+
+#region 2034
+{
+    StockPrice stockPrice = new StockPrice();
+    stockPrice.Update(1, 10);
+    stockPrice.Update(2, 5);
+    Console.WriteLine(stockPrice.Current());
+    Console.WriteLine(stockPrice.Minimum());
+    Console.WriteLine(stockPrice.Maximum());
+    stockPrice.Update(1, 3);
+    Console.WriteLine(stockPrice.Current());
+    Console.WriteLine(stockPrice.Minimum());
+    Console.WriteLine(stockPrice.Maximum());
+    stockPrice.Update(1, 2);
+    stockPrice.Update(2, 2);
+    stockPrice.Update(3, 2);
+    stockPrice.Update(4, 2);
+    stockPrice.Update(5, 2);
+    stockPrice.Update(6, 2);
+    stockPrice.Update(6, 3);
+    stockPrice.Update(6, 4);
+    stockPrice.Update(6, 1);
+    stockPrice.Update(6, 2);
+    stockPrice.Update(6, 4);
+    Console.WriteLine(stockPrice.Current());
+    Console.WriteLine(stockPrice.Minimum());
+    Console.WriteLine(stockPrice.Maximum());
+}
+#endregion
 
 Console.ReadKey();
