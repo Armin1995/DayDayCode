54015c9ef9bd71f9703bf024aeb04b8ee31d3d29
Armin君 <aichenzhupo127@163.com>
2023-10-14 18:18:07 +0800

    136. 只出现一次的数字 用异或

---
+++ DayDayLeetCode/136/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayDayLeetCode._136
+{
+    /// <summary>
+    /// 136. 只出现一次的数字
+    /// 巧妙运用异或的性质 0异或a=a a异或a=0 异或满足交换律和结合律
+    /// </summary>
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int ret = 0;
+            foreach (int e in nums) ret ^= e;
+            return ret;
+        }
+    }
+}
