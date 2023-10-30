27fecd2657158da8719649449d71a9ffe330b768
Armin君 <aichenzhupo127@163.com>
2023-10-30 22:55:41 +0800

    275. H 指数 II 二分法

---
+++ DayDayLeetCode/275/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayDayLeetCode._275
+{
+    /// <summary>
+    /// 275. H 指数 II
+    /// </summary>
+    public class Solution
+    {
+        public int HIndex(int[] citations)
+        {
+            int n = citations.Length;
+            int left = 0, right = n - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (citations[mid] >= n - mid)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return n - left;
+        }
+    }
+}
---
+++ DayDayLeetCodeTests/275/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DayDayLeetCode._275;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayDayLeetCode._275.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void HIndexTest()
+        {
+            Solution solution = new Solution();
+            Assert.AreEqual(solution.HIndex(new int[] { 0, 1, 3, 5, 6 }), 3);
+            Assert.AreEqual(solution.HIndex(new int[] { 1, 2, 100 }), 2);
+            Assert.AreEqual(solution.HIndex(new int[] { 1, 1, 1, 1, 2, 100 }), 2);
+        }
+    }
+}
