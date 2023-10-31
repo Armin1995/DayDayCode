fbc9b22c6b463ba36d35d409e3c8acac3f25cf25
Armin君 <aichenzhupo127@163.com>
2023-10-31 22:37:35 +0800

    2003. 每棵子树内缺失的最小基因值

---
+++ DayDayLeetCodeTests/2003/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DayDayLeetCode._2003;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayDayLeetCode._2003.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void SmallestMissingValueSubtreeTest()
+        {
+            Solution solution = new Solution();
+            var result = solution.SmallestMissingValueSubtree(new int[] { -1, 2, 3, 0, 2, 4, 1 }, new int[] { 2, 3, 4, 5, 6, 7, 8 });
+            Assert.IsTrue(result.SequenceEqual(new int[] { 1, 1, 1, 1, 1, 1, 1 }));
+        }
+    }
+}
