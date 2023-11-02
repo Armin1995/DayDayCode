799c732fb25c1d8026e7b3db18b3472fb76e4a43
Armin君 <aichenzhupo127@163.com>
2023-11-02 21:34:10 +0800

    2103. 环和杆 简单题

---
+++ DayDayLeetCode/2103/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayDayLeetCode._2103
+{
+    /// <summary>
+    /// 2103. 环和杆
+    /// </summary>
+    public class Solution
+    {
+        public int CountPoints(string rings)
+        {
+            int[,] array = new int[10, 3];
+
+            for (int i = 0; i < rings.Length; i += 2)
+            {
+                if (rings[i] == 'R')
+                {
+                    array[rings[i + 1] - '0', 0] = 1;
+                }
+                else if (rings[i] == 'G')
+                {
+                    array[rings[i + 1] - '0', 1] = 1;
+                }
+                else
+                {
+                    array[rings[i + 1] - '0', 2] = 1;
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (array[i, 0] == 1 && array[i, 1] == 1 && array[i, 2] == 1)
+                {
+                    sum++;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ DayDayLeetCodeTests/2103/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DayDayLeetCode._2103;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayDayLeetCode._2103.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void CountPointsTest()
+        {
+            Solution solution = new Solution();
+            Assert.AreEqual(solution.CountPoints("B0B6G0R6R0R6G9"),1);
+            Assert.AreEqual(solution.CountPoints("B0R0G0R9R0B0G0"),1);
+        }
+    }
+}
