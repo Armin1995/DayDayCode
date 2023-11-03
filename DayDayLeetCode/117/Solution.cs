da81f4aad94aa9cccc8884cee5fb3e267c422df1
Armin君 <aichenzhupo127@163.com>
2023-11-03 21:51:16 +0800

    117 注意只能用常数级别的额外空间

---
+++ DayDayLeetCode/117/Solution.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DayDayLeetCode._117
+{
+    public class Solution
+    {
+        public Node Connect(Node root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+
+            var nodes = new Queue<Node>();
+            nodes.Enqueue(root);
+
+            while (nodes.Count > 0)
+            {
+                var count = nodes.Count;
+                Node pre = null;
+
+                for (int i = 0; i < count; i++)
+                {
+                    var node = nodes.Dequeue();
+
+                    if (pre != null)
+                    {
+                        pre.next = node;
+                    }
+
+                    pre = node;
+
+                    if (node.left != null)
+                    {
+                        nodes.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        nodes.Enqueue(node.right);
+                    }
+                }
+            }
+
+            return root;
+        }
+    }
+
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next)
+        {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+}
---
+++ DayDayLeetCodeTests/117/SolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DayDayLeetCode._117;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayDayLeetCode._117.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void ConnectTest()
+        {
+            Node node4 = new Node(4);
+            Node node5 = new Node(5);
+            Node node7 = new Node(7);
+            Node node2 = new Node(2, node4, node5, null);
+            Node node3 = new Node(3, null, node7, null);
+            Node node1 = new Node(1, node2, node3, null);
+
+            Solution solution = new Solution();
+            var node = solution.Connect(node1);
+        }
+    }
+}
