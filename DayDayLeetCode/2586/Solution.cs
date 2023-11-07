00f208944282f5d1f0bda366e64a59519d965206
Armin君 <aichenzhupo127@163.com>
2023-11-07 21:56:37 +0800

    2586. 统计范围内的元音字符串数 简单题

---
+++ DayDayLeetCode/2586/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayDayLeetCode._2586
+{
+    public class Solution
+    {
+        public int VowelStrings(string[] words, int left, int right)
+        {
+            int count = 0;
+
+            for (int i = left; i <= right; i++)
+            {
+                if ((words[i][0] == 'a' || words[i][0] == 'e' || words[i][0] == 'i' || words[i][0] == 'o' || words[i][0] == 'u') && (words[i][words[i].Length-1] == 'a' || words[i][words[i].Length - 1] == 'e' || words[i][words[i].Length - 1] == 'i' || words[i][words[i].Length - 1] == 'o' || words[i][words[i].Length - 1] == 'u'))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
