3d54b29d52fee5b00e2a767ffd1ad52aa5a90605
Armin君 <aichenzhupo127@163.com>
2023-11-08 22:04:30 +0800

    2609. 最长平衡子字符串

---
+++ DayDayLeetCode/2609/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayDayLeetCode._2609
+{
+    /// <summary>
+    /// 2609. 最长平衡子字符串
+    /// </summary>
+    public class Solution
+    {
+        public int FindTheLongestBalancedSubstring(string s)
+        {
+            int zeroCount = 0;
+            int oneCount = 0;
+
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '0')
+                {
+                    if (oneCount == 0)
+                    {
+                        zeroCount++;
+                    }
+                    else
+                    {
+                        count = Math.Max(count, Math.Min(zeroCount, oneCount));
+                        zeroCount = 1;
+                        oneCount = 0;
+                    }
+                }
+                else
+                {
+                    oneCount++;
+                }
+            }
+
+            count = Math.Max(count, Math.Min(zeroCount, oneCount));
+
+            return count * 2;
+        }
+    }
+}
---
+++ DayDayLeetCodeTests/2609/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DayDayLeetCode._2609;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayDayLeetCode._2609.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void FindTheLongestBalancedSubstringTest()
+        {
+            Solution solution = new Solution();
+            Assert.AreEqual(solution.FindTheLongestBalancedSubstring("01000111"), 6);
+            Assert.AreEqual(solution.FindTheLongestBalancedSubstring("00111"), 4);
+            Assert.AreEqual(solution.FindTheLongestBalancedSubstring("111"), 0);
+        }
+    }
+}
