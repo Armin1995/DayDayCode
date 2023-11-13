6ec4020b203ec94ec9eb5247c04be3149376e035
Armin君 <aichenzhupo127@163.com>
2023-11-13 22:03:09 +0800

    307 数组更新和检索

---
+++ DayDayLeetCode/307/NumArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayDayLeetCode._307
+{
+    public class NumArray
+    {
+        private int[] nums;
+
+        public NumArray(int[] nums)
+        {
+            this.nums = nums;
+        }
+
+        public void Update(int index, int val)
+        {
+            nums[index] = val;
+        }
+
+        public int SumRange(int left, int right)
+        {
+            int sum = 0;
+
+            for (int i = left; i <= right; i++)
+            {
+                sum += nums[i];
+            }
+
+            return sum;
+        }
+    }
+}
