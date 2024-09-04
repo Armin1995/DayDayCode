1c94d2923735494c61f154d82b8f2c3753e3a327
Armin君 <aichenzhupo127@163.com>
2024-09-04 23:24:13 +0800

    重新提交

---
+++ DayDayQuant/TuShare/Model/RESTfulModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayDayQuant.TuShare.Model
+{
+    internal class RESTfulModel
+    {
+        #region 字段
+
+        #endregion
+
+        #region 属性
+        public string API_Name { get; set; } = "daily";
+        public string Token { get; set; } = "";
+        public string Params { get; set; }
+        public string Fields { get; set; }
+        #endregion
+
+        #region 方法
+        public RESTfulModel()
+        {
+
+        }
+        #endregion
+
+    }
+}
