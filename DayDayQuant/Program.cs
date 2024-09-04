ec8f4f9646de84f4b72df7a6d38886c02fccfa94
Armin君 <aichenzhupo127@163.com>
2024-09-04 23:25:44 +0800

    清理无效项目

--- DayDayQuant/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-string uri = "";
-HttpClient client = new HttpClient();
-//client.GetAsync();
